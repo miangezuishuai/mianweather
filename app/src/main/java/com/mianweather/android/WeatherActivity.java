a995c5a334b920194560c8c1e7b471cedf18c29b
MianJ <297885703@qq.com>
2018-12-01 16:40:02 +0800

    增加后台自动更新天气的功能

--- app/src/main/java/com/mianweather/android/WeatherActivity.java
+++ app/src/main/java/com/mianweather/android/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.mianweather.android;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -22,6 +23,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.mianweather.android.gson.Forecast;
 import com.mianweather.android.gson.Weather;
+import com.mianweather.android.service.AutoUpdateService;
 import com.mianweather.android.util.HttpUtil;
 import com.mianweather.android.util.Utility;
 
@@ -207,6 +209,8 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 
     /**
